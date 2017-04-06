b2d48120da06b15f62210f8ecf0825380c3f4027
jinhao <jinhao_@hotmail.com>
2017-04-06 23:43:27 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+
+path = r'C:\Users\Chan Family\Desktop\pythonpoint\slides.txt'
+
+mainpoint = '1'
+
+with open(path,'r') as fin:
+    read_data = fin.readlines()
+    for line in read_data:
+        m = re.search(r'\[([^\]]+)\]\(([^\)]+)\)', line)
+        mark = m.group(1)
+        content = m.group(2)
+        if mark == "h": # if its the header
+            print  content 
+            print (len(content) + 4) * "#"
+        else:
+            point = mark.split(".")
+            if point[0] == mainpoint: # if a sub point of previous point
+                print mainpoint + "." + ("~"*(len(mark)-1)) + content
+            else:
+                mainpoint = point[0]
+                print mainpoint + "." + ("~"*(len(mark)-1)) + content
+
+import pdb; pdb.set_trace()
+            
