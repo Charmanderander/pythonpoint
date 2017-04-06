e1ba1df2333d05476e33f31135e021d4c419cbbc
jinhao <jinhao_@hotmail.com>
2017-04-06 23:53:29 +0800

    changed comment to match the code

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ with open(path,'r') as fin:
             print (len(content) + 4) * "#"
         else:
             point = mark.split(".")
-            if point[0] != mainpoint: # if a sub point of previous point
+            if point[0] != mainpoint: # if not a sub point of previous point
                 mainpoint = point[0]
                 
             print mainpoint + "." + ("~"*(len(mark)-1)) + content
