953ff4d7387f18dad63aadd500e13c6a213429d7
jinhao <jinhao_@hotmail.com>
2017-04-08 13:35:31 +0800

    working version

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import re
+import code
+import os, os.path
 
-path = r'slides.txt'
+DIR = "slides/"
 
+slide_count = 1
+total_slides = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+path = r'slides/slide' + str(slide_count) + '.txt'
 
-with open(path,'r') as fin:
-    read_data = fin.readlines()
-    for line in read_data:
-        m = re.search(r'\[([^\]]+)\]\(([^\)]+)\)', line)
-        mark = m.group(1)
-        content = m.group(2)
-        if mark == "h": # if its the header
-            print  content 
-            print (len(content) + 4) * "#"
+while(input != "3"):
+    with open(path,'r') as fin:
+        read_data = fin.readlines()
+        for line in read_data:
+            # getting values in the brackets [] and ()
+            # values in () indicates which point
+            # values in [] represents the content
+            m = re.search(r'\[([^\]]+)\]\(([^\)]+)\)', line)
+            mark = m.group(1)
+            content = m.group(2)
+            if mark == "h": # if its the header
+                print "\n" * 100
+                print 80 * "^"
+                print "Slide " + str(slide_count) + "/" + str(total_slides)
+                print  content 
+                print 80 * "-"
+            else: # else print the contents
+                point = mark.split(".")
+                # len(mark) finds out which subpoint is it
+                print point[0] + "." + (" "*(len(mark)-1)) + "~ " +content
+
+    code.interact(local=locals())
+    input = raw_input("Next Slide: 1\nPrev Slide: 2\nEnd Presentation: 3\nChoice:")
+    if input == "1":
+        slide_count += 1
+        new_slide = r'slides/slide' + str(slide_count) + '.txt'
+        if os.path.exists(new_slide):
+            path = new_slide
         else:
-            point = mark.split(".")                
-            print point[0] + "." + ("~"*(len(mark)-1)) + content
+            r'slide' + str(slide_count - 1) + '.txt'
+            slide_count -= 1
+    elif input == "2":
+        slide_count -= 1
+        if slide_count == 0:
+            slide_count += 1
+        
 
-import pdb; pdb.set_trace()
-            
+
+print "Thank you!"
